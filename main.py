ce8a56b4d42124e806c432fb166b8a1a4c702daf
HeyHo-bit <57176916+HeyHo-bit@users.noreply.github.com>
2024-04-19 18:24:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+fizznumbers = list(range(1, 101))
+
+def Fizzbuzz():
+    for i in fizznumbers:
+        if i % 3 == 0 and i % 5 == 0 :
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == '__main__':
+
+    Fizzbuzz()
+    
